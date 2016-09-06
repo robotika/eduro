a99ef59ad5bac7ed7af9a0b18b8f636b1a7e5228
martind <martind@mapfactor.com>
2016-09-06 20:14:22 +0200

    SICK ver0

--- sickday2016.py
+++ sickday2016.py
@@ -31,12 +31,22 @@ from ray_trace import combinedPose
 from line import distance, Line
 from route import loadLatLonPts, Convertor
 
+from sdoplg import ReadSDO, WriteSDO
+
 
 import starter
 
 
 def setupGripperModule(can): 
-    pass  # TODO setup the servo PWM output
+    writer = WriteSDO( 0x7F, 0x2100, 1, [0xF] )  # enable servos
+    for cmd in writer.generator():
+        if cmd:
+            can.sendData( *cmd )
+        writer.update( can.readPacket() ) 
+
+
+def gripperServo(can, left, right):
+    can.sendData( 0x37F, [0,0,0,0, left & 0xFF, (left >> 8)&0xFF, right & 0xFF, (right >> 8)&0xFF] )  
 
 
 class SICKRobotDay2016:
@@ -48,10 +58,11 @@ class SICKRobotDay2016:
         self.robot.attachEmergencyStopButton()
 
         # do we want some processing?
-        self.robot.attachCamera( cameraExe = "../digits/digits", 
-                url = "http://192.168.0.99/image?res=full&x0=352&y0=80&x1=992&y1=592&quality=12&doublescan=0" )
+#        self.robot.attachCamera( cameraExe = "../digits/digits", 
+#                url = "http://192.168.0.99/image?res=full&x0=352&y0=80&x1=992&y1=592&quality=12&doublescan=0" )
+        self.robot.attachCamera(sleep=0.5)
 #        self.robot.addExtension( cameraDataExtension )
-        self.robot.attachLaser( pose=((0.14, 0.0, 0.32), (0,0,0)) )
+#        self.robot.attachLaser( pose=((0.14, 0.0, 0.32), (0,0,0)) )
 #        self.robot.attachLaser( index=2, remission=True, usb=True, 
 #                pose=((0.19, 0.0, 0.055), tuple([math.radians(x) for x in (0, 180, 0)])), 
 #                errLog = self.robot.metaLog )
@@ -59,17 +70,17 @@ class SICKRobotDay2016:
         self.driver = Driver( self.robot, maxSpeed = 0.5, maxAngularSpeed = math.radians(180) )
         self.robot.localisation = SimpleOdometry()
         
-        self.robot.laser.stopOnExit = False    # for faster boot-up
+#        self.robot.laser.stopOnExit = False    # for faster boot-up
 
 
     def run( self ):
         try:
-            if getattr( self.robot.laser, 'startLaser', None ):
+#            if getattr( self.robot.laser, 'startLaser', None ):
                 # trigger rotation of the laser, low level function, ignore for log files
-                print "Powering laser ON"
-                self.robot.laser.startLaser() 
+#                print "Powering laser ON"
+#                self.robot.laser.startLaser() 
             self.robot.waitForStart()
-            self.robot.laser.start()    # laser also after start -- it should be already running
+#            self.robot.laser.start()    # laser also after start -- it should be already running
 #            self.robot.laser2.start() 
             self.robot.camera.start()
             self.robot.localisation = SimpleOdometry()
@@ -77,7 +88,7 @@ class SICKRobotDay2016:
                 self.ver0(verbose = self.verbose)            
         except EmergencyStopException, e:
             print "EmergencyStopException"
-        self.robot.laser.requestStop()
+#        self.robot.laser.requestStop()
 #        self.robot.laser2.requestStop() 
         self.robot.camera.requestStop()
 
@@ -86,7 +97,12 @@ class SICKRobotDay2016:
         # Go straight for 2 meters
         print "ver0", self.robot.battery
 
+        gripperServo(self.robot.can, 32512, 51200)  # open
+        self.driver.goStraight(0.3, timeout=30)
+        gripperServo(self.robot.can, 43520, 32512)  # close
         self.driver.goStraight(2.0, timeout=30)
+        gripperServo(self.robot.can, 32512, 51200)  # open
+        self.driver.goStraight(-0.3, timeout=30)
 
         print 'Game over.', self.robot.battery
         for k in xrange(10):
